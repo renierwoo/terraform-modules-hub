a7095db3e8fe0bfb0b3c26661603daf3b2f8c23f
Renier Collazo Woo <rcollazo@emergya.com>
2023-06-02 15:49:28 -0400

    Add AWS EKS module versions.tf file

---
+++ AWS/modules/eks/versions.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.4.6"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.1.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 4.0.4"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.21.0"
+    }
+    time = {
+      source = "hashicorp/time"
+      version = ">= 0.9.1"
+    }
+  }
+}
